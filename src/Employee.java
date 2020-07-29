9e48770b45215e848df86d7be0face73df165ee2
ulempicka <urszula.lempicka@gmail.com>
2020-07-29 18:06:45 +0200

    commit 2 add hello and employee

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    public final String name;
+    public final String lastName;
+
+    public Employee(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
